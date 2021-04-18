1e22c39bd0e25ed87ba650fc708aab3cc63a9f84
Vincent329 <vincee.ho@gmail.com>
2021-04-17 21:16:38 -0400

    [ADD] Movement logic

--- VincentHo_GAME3002_A3/Assets/Scripts/PlayerMovement.cs
+++ VincentHo_GAME3002_A3/Assets/Scripts/PlayerMovement.cs
@@ -8,14 +8,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float m_fSpeedValue;
     [SerializeField] private float moveValue;
     [SerializeField] private float m_fJumpForce;
+    [SerializeField] private Vector3 m_vel;
+
+    [SerializeField] private float m_fMonitorSpeed;
 
     // modifiable key placed in the editor (set as Space)
     [SerializeField] private KeyCode m_Jump;
 
     // checks if the player is grounded
     [SerializeField] private bool m_bCanJump;
-
-    [SerializeField] 
 
     private Rigidbody m_rb;
 
@@ -35,21 +36,46 @@ public class PlayerMovement : MonoBehaviour
     private void HandleInputs()
     {
         moveValue = Input.GetAxisRaw("Horizontal");
-        Vector3 m_vel = new Vector3(moveValue, 0.0f);
-        m_rb.AddForce(m_vel, ForceMode.Force);
-        //m_rb.velocity = new Vector3(moveValue * m_fSpeedValue, m_rb.velocity.y, 0); 
-        
+        m_vel = new Vector3(moveValue * m_fSpeedValue, 0.0f, 0.0f);
+        //m_rb.AddForce(m_vel, ForceMode.VelocityChange);
+
         if (Input.GetKeyDown(m_Jump) && m_bCanJump)
         {
             m_rb.AddForce(Vector3.up * m_fJumpForce, ForceMode.Impulse);
+
             m_bCanJump = false;
         }
     }
 
     private void FixedUpdate()
     {
-        
+        //if (m_rb.velocity.magnitude > m_fSpeedValue)
+        //{
+        //    m_rb.velocity *= 0.75f;
+        //}
+        AddForceToVelocity(m_rb, m_vel, 10000.0f);
     }
+
+    private void AddForceToVelocity(Rigidbody rb, Vector3 maxVelocity, float AppForce = 1, ForceMode mode = ForceMode.Force)
+    {
+        if (AppForce == 0 || maxVelocity.magnitude == 0)
+            return;
+
+        maxVelocity = maxVelocity + maxVelocity.normalized * 0.2f * rb.drag;
+
+        // this could be any value really
+        AppForce = Mathf.Clamp(AppForce, -rb.mass/Time.fixedDeltaTime, rb.mass/Time.fixedDeltaTime);
+
+        if (rb.velocity.magnitude == 0)
+        {
+            rb.AddForce(maxVelocity * AppForce, mode);
+        } else
+        {
+            Vector3 velocityProjectedToTarget = (maxVelocity.normalized * Vector3.Dot(maxVelocity, rb.velocity) / maxVelocity.magnitude);
+            rb.AddForce((maxVelocity - velocityProjectedToTarget) * AppForce, mode);
+        }
+    }
+    
 
     private void OnCollisionEnter(Collision other)
     {
